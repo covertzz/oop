be1fdfd0411daf97f6cd67500d96fd22407e9fc1
covertzz <118168972+covertzz@users.noreply.github.com>
2023-11-28 00:52:17 -0800

    Update iterator.java

--- iterator/iterator.java
+++ iterator/iterator.java
@@ -1,22 +1,33 @@
+/*
+ * Authors: Palmer Du, Jason Yam
+ * Class: CS3560 Object Oriented Programming
+ * Description: This program showcases the iterator design method which involves the implementation 
+ *              of next() and hasNext() methods to cycle through a collection of objects.
+ */
+
 import java.util.*;
 
+//Main executes our iterator class design
 class Main {
     public static void main(String[] args) {
         CollectionIterator i = new CollectionIterator();
-       
-        Object currentObj = i.next();
+        Object currentObj; //Create CollectionIterator object 
 
-        while(i.hasNext()) {
-            System.out.println(currentObj);
+        //While loop runs while CollectionIterator contains objects
+        while(i.hasNext()) { 
             currentObj = i.next();
+            System.out.println(currentObj); //Iterate through next object 
+             //Print out object
         }
     }
 }
 
 class Collection {
-    public ArrayList<Object> collection = new ArrayList<>();
+    public ArrayList<Object> collection = new ArrayList<>(); //All objects have an arraylist variable "collection"
 
+    //Constructor
     Collection() {
+        //Add items to our ArrayList
         collection.add("hello");
         collection.add("goodbye");
         collection.add(1);
@@ -24,29 +35,39 @@ class Collection {
         collection.add(true);
     }
 
+    //returns a reference to ArrayList collection
     public ArrayList<Object> getCollection() {
         return collection;
     }       
 }
 
+//next() and hasNext() are the two core methods 
 interface Iterator {
     public Object next();
     public boolean hasNext();
 }
 
+//generic iterator for a collection of Objects 
 class CollectionIterator implements Iterator {
-    Collection c = new Collection();
-    int currentItem = 0;
-    
+    Collection c = new Collection(); //new instance
+    int currentItem = 0; //to keep track of position in collection
+
+    //returns the current item if hasNext() is true, otherwise returns null; always use with hasNext()
     public Object next() {
-        currentItem++;
         if(hasNext()) {
-            return c.getCollection().get(currentItem-1);
+            Object result = c.getCollection().get(currentItem); //get current item
+            currentItem++; //increment 
+            return result; 
         }
         return null;
     }
 
+    //checks if the position of currentItem is greater than the collection's size to determine hasNext()
     public boolean hasNext() {
-        return currentItem <= c.getCollection().size();
+        if (currentItem < c.getCollection().size()) {   //if current item is less than the size of the array
+            return true;
+        }
+        return false;
     }
+        
 }
